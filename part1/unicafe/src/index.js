a482b3cc32d71f19c5c3bf038be9d9eb3880e7b3
Alex Akulenko <oleksandr.akulenko0@gmail.com>
2019-10-31 23:58:08 +0200

    Add three buttons and functionality to increase values using buttons.
    Remove index.css

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
 
 const App = () => {
   // save clicks of each button to own state
@@ -10,7 +15,17 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+
+      <Button handleClick={() => setGood(good + 1)} text='good'/>
+      <Button handleClick={() => setNeutral(neutral + 1)} text='neutral'/>
+      <Button handleClick={() => setBad(bad + 1)} text='bad'/>
+
+      <h1>statistics</h1>
+      
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   )
 }
