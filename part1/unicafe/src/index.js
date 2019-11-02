81fbad6a80f8723f33ddbc4cecdad65fa54042db
Alex Akulenko <oleksandr.akulenko0@gmail.com>
2019-11-02 23:30:42 +0200

    Add all, average and positive feedback. WIP.

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -7,11 +7,30 @@ const Button = ({ handleClick, text }) => (
   </button>
 )
 
+const Average = ({ good, bad, all }) => {
+  if (all > 0) {
+    return <p>average {(good * 1 + bad * (-1))/all}</p>
+  } else {
+    return <p>average 0</p>
+  }
+}
+
+const PositiveFeedback = ({ good, all }) => {
+  if (all > 0) {
+    return <p>positive {good*100/all} %</p>
+  } else {
+    return <p>positive 0 %</p>
+  }
+}
+
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
+  const all = good + neutral + bad
 
   return (
     <div>
@@ -26,6 +45,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <Average good={good} neutral={neutral} bad={bad} all={all}/>
+      <PositiveFeedback good={good} all={all}/>
     </div>
   )
 }
